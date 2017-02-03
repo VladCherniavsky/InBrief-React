34220ab33300d4043d52ebfb96a72d4d70d66c00
vladbitgroup <vlad@bitgroup.co.il>
2017-02-03 18:58:36 +0300

    test material ui

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import {Link} from 'react-router';
+import RaisedButton from 'material-ui/RaisedButton'
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {deepOrange500} from 'material-ui/styles/colors';
+import Paper from 'material-ui/Paper';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import AppBar from 'material-ui/AppBar';
+const style = {
+    display: 'inline-block',
+    margin: '16px 32px 16px 0',
+};
+
+const muiTheme = getMuiTheme({
+    palette: {
+        accent1Color: deepOrange500,
+    },
+});
 
 class HomePage extends React.Component {
     render() {
@@ -9,6 +27,32 @@ class HomePage extends React.Component {
                 <Link to="/short/about" className="btn btn-primary btn-lg">
                     About
                 </Link>
+                <MuiThemeProvider muiTheme={muiTheme}>
+
+                    <div>
+                        <AppBar
+                            title="Title"
+                            iconClassNameRight="muidocs-icon-navigation-expand-more"
+                        />
+                        <Paper style={style}>
+                            <Menu>
+                                <MenuItem primaryText="Maps" />
+                                <MenuItem primaryText="Books" />
+                                <MenuItem primaryText="Flights" />
+                                <MenuItem primaryText="Apps" />
+                            </Menu>
+                        </Paper>
+                        <Paper style={style}>
+                            <Menu>
+                                <MenuItem primaryText="Refresh" />
+                                <MenuItem primaryText="Help &amp; feedback" />
+                                <MenuItem primaryText="Settings" />
+                                <MenuItem primaryText="Sign out" />
+                            </Menu>
+                        </Paper>
+                    </div>
+                </MuiThemeProvider>
+
             </div>
         );
     }
