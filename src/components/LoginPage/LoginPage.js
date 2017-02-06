e04798f540f5c0cafd23c0dea220f656374aa288
vladbitgroup <vlad@bitgroup.co.il>
2017-02-06 19:29:42 +0300

    getting familiar with material ui

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -2,16 +2,41 @@
  * Created by vlad on 9/29/2016.
  */
 import React from 'react';
-import styles from './style.scss';
+import stylesScss from './style.scss';
+import TextField from 'material-ui/TextField';
+import {orange500, blue500, deepPurple900, grey50, lightGreenA700} from 'material-ui/styles/colors';
 
+const styles = {
+    errorStyle: {
+        color: orange500
+    },
+    underlineStyle: {
+        borderColor: lightGreenA700
+    },
+    floatingLabelStyle: {
+        color: grey50
+    },
+    floatingLabelFocusStyle: {
+        color: blue500
+    }
+};
 class LoginPage extends React.Component {
-
-
     render() {
-        console.log('styles', styles);
         return (
-            <div className={styles.loginPage}>
-                fff
+            <div className={stylesScss.loginPage}>
+                <form action="">
+                    <TextField
+                        hintText="Message Field"
+                        hintStyle={styles.errorStyle}
+                        errorText="This field is required"
+
+                        floatingLabelStyle={styles.floatingLabelStyle}
+                        underlineFocusStyle={styles.underlineStyle}
+                        floatingLabelText="MultiLine and FloatingLabel"
+                        rows={1}
+                    /><br />
+
+                </form>
 
             </div>
         );
