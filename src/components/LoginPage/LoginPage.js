19bd3df3939d3a979b1fda6823996775cbfe2a10
vladbitgroup <vlad@bitgroup.co.il>
2017-02-07 16:28:12 +0300

    styled login page

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -4,44 +4,71 @@
 import React from 'react';
 import stylesScss from './style.scss';
 import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import Lock from 'material-ui/svg-icons/action/lock'
 import {
-    orange500,
     blue500,
     grey50,
-    lightGreenA700
+    grey500
 } from 'material-ui/styles/colors';
 
 const styles = {
-    errorStyle: {
-        color: orange500
+   hintStyle: {
+        color: grey500
     },
     underlineStyle: {
-        borderColor: lightGreenA700
+        borderColor: blue500
     },
     floatingLabelStyle: {
         color: grey50
     },
     floatingLabelFocusStyle: {
         color: blue500
+    },
+    button: {
+        color: 'white'
+    },
+    input: {
+        color: 'white'
     }
 };
 class LoginPage extends React.Component {
     render() {
         return (
             <div className={stylesScss.loginPage}>
-                <form action="">
-                    <TextField
-                        hintText="Message Field"
-                        hintStyle={styles.errorStyle}
-                        errorText="This field is required"
+                    <form className={stylesScss.form}>
+                        <div className={stylesScss.fields}>
+                            <TextField
+                                hintText="Please enter email"
+                                hintStyle={styles.hintStyle}
+                                floatingLabelStyle={styles.floatingLabelStyle}
+                                floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
+                                underlineFocusStyle={styles.underlineStyle}
+                                floatingLabelText="Email"
+                                rows={1}
+                                inputStyle={styles.input}
+                            /><br />
+                            <TextField
+                                hintText="Please enter password"
+                                hintStyle={styles.hintStyle}
+                                floatingLabelStyle={styles.floatingLabelStyle}
+                                floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
+                                underlineFocusStyle={styles.underlineStyle}
+                                floatingLabelText="Password"
+                                inputStyle={styles.input}
+                                type="password"
+                                rows={1}
+                            /><br />
+                            <RaisedButton
+                                label="Login"
+                                labelPosition="after"
+                                backgroundColor={blue500}
+                                fullWidth={true}
+                                icon={<Lock />}
+                                labelColor={styles.button.color}/>
+                        </div>
+                    </form>
 
-                        floatingLabelStyle={styles.floatingLabelStyle}
-                        underlineFocusStyle={styles.underlineStyle}
-                        floatingLabelText="MultiLine and FloatingLabel"
-                        rows={1}
-                    /><br />
-
-                </form>
 
             </div>
         );
