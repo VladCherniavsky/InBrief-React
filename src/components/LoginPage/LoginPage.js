7f876967149b7387abd9b56291c7abe2c2ec517f
vladbitgroup <vlad@bitgroup.co.il>
2017-02-08 18:31:35 +0300

    little changes

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -5,7 +5,7 @@ import React from 'react';
 import stylesScss from './style.scss';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
-import Lock from 'material-ui/svg-icons/action/lock'
+import Lock from 'material-ui/svg-icons/action/lock';
 import {
     blue500,
     grey50,
@@ -13,7 +13,7 @@ import {
 } from 'material-ui/styles/colors';
 
 const styles = {
-   hintStyle: {
+    hintStyle: {
         color: grey500
     },
     underlineStyle: {
@@ -36,38 +36,42 @@ class LoginPage extends React.Component {
     render() {
         return (
             <div className={stylesScss.loginPage}>
-                    <form className={stylesScss.form}>
-                        <div className={stylesScss.fields}>
-                            <TextField
-                                hintText="Please enter email"
-                                hintStyle={styles.hintStyle}
-                                floatingLabelStyle={styles.floatingLabelStyle}
-                                floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
-                                underlineFocusStyle={styles.underlineStyle}
-                                floatingLabelText="Email"
-                                rows={1}
-                                inputStyle={styles.input}
-                            /><br />
-                            <TextField
-                                hintText="Please enter password"
-                                hintStyle={styles.hintStyle}
-                                floatingLabelStyle={styles.floatingLabelStyle}
-                                floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
-                                underlineFocusStyle={styles.underlineStyle}
-                                floatingLabelText="Password"
-                                inputStyle={styles.input}
-                                type="password"
-                                rows={1}
-                            /><br />
-                            <RaisedButton
-                                label="Login"
-                                labelPosition="after"
-                                backgroundColor={blue500}
-                                fullWidth={true}
-                                icon={<Lock />}
-                                labelColor={styles.button.color}/>
-                        </div>
-                    </form>
+                <form className={stylesScss.form}>
+                    <div className={stylesScss.fields}>
+                        <TextField
+                            hintText="Please enter email"
+                            hintStyle={styles.hintStyle}
+                            floatingLabelStyle={styles.floatingLabelStyle}
+                            floatingLabelFocusStyle={
+                                    styles.floatingLabelFocusStyle
+                                }
+                            underlineFocusStyle={styles.underlineStyle}
+                            floatingLabelText="Email"
+                            rows={1}
+                            inputStyle={styles.input}
+                        /><br />
+                        <TextField
+                            hintText="Please enter password"
+                            hintStyle={styles.hintStyle}
+                            floatingLabelStyle={styles.floatingLabelStyle}
+                            floatingLabelFocusStyle={
+                                    styles.floatingLabelFocusStyle
+                                }
+                            underlineFocusStyle={styles.underlineStyle}
+                            floatingLabelText="Password"
+                            inputStyle={styles.input}
+                            type="password"
+                            rows={1}
+                        /><br />
+                        <RaisedButton
+                            label="Login"
+                            labelPosition="after"
+                            backgroundColor={blue500}
+                            fullWidth={true}
+                            icon={<Lock />}
+                            labelColor={styles.button.color}/>
+                    </div>
+                </form>
 
 
             </div>
