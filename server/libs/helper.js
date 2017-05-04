0927fe31df65eaf70599020f045937801eb31f6a
vladbitgroup <vlad@bitgroup.co.il>
2017-05-04 15:23:09 +0300

    refactor findAndCount

--- server/libs/helper.js
+++ server/libs/helper.js
@@ -8,23 +8,22 @@
  * @param {Object} filter - Object which will be used as selector
  * @param {Object} skipLimit - Specify take and skip parameter
  * @param {Object} sort - Specify sorting
- * @param {String} name - Optional: set property name in returned object(default: 'data')
  * @return {Object} data - Contains 'count'  and 'data' properties
  */
-export async function findAndCount(filter, skipLimit, sort, name) {
-    const select = filter ? filter : {};
-    const skipAndLimit = skipLimit ? skipLimit : null;
-    const sortProps = sort ? sort : null;
+const findAndCount = async (filter = {}, skipLimit = null, sort = null) => {
     try {
-        const data = await Promise.all([
+        const [count, data] = await Promise.all([
             this.count(filter),
-            this.find(select, null, skipAndLimit).sort(sortProps)
+            this.find(filter, null, skipLimit).sort(sort)
         ]);
         return {
-            count: data[0],
-            [name ? name : 'data']: data[1]
+            count,
+            data
         };
     } catch(err) {
         throw err;
     }
-}
+};
+
+export findAndCount;
+
